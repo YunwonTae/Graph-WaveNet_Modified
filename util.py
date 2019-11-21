baca3272ed159dc8e8bdfa0701e52d9c5bffd1c5
YunwonTae <ytae@ucdavis.edu>
2019-11-22 08:59:38 +0900

    Update util.py

--- util.py
+++ util.py
@@ -98,6 +98,112 @@ class Incident_DataLoader(object):
 
         return _wrapper()
 
+class Weather_DataLoader(object):
+    def __init__(self, xs, ys, weather, batch_size, pad_with_last_sample=True):
+        """
+        :param xs:
+        :param ys:
+        :param incident:
+        :param batch_size:
+        :param pad_with_last_sample: pad with the last sample to make number of samples divisible to batch_size.
+        """
+        self.batch_size = batch_size
+        self.current_ind = 0
+        if pad_with_last_sample:
+            num_padding = (batch_size - (len(xs) % batch_size)) % batch_size
+            x_padding = np.repeat(xs[-1:], num_padding, axis=0)
+            y_padding = np.repeat(ys[-1:], num_padding, axis=0)
+            weather_padding = np.repeat(weather[-1:], num_padding, axis=0)
+
+            xs = np.concatenate([xs, x_padding], axis=0)
+            ys = np.concatenate([ys, y_padding], axis=0)
+            weather = np.concatenate([weather, weather_padding], axis=0)
+
+        self.size = len(xs)
+        self.num_batch = int(self.size // self.batch_size)
+        self.xs = xs
+        self.ys = ys
+        self.weather = weather
+
+    def shuffle(self):
+        permutation = np.random.permutation(self.size)
+        xs, ys, weather= self.xs[permutation], self.ys[permutation], self.weather[permutation]
+        self.xs = xs
+        self.ys = ys
+        self.weather = weather
+
+    def get_iterator(self):
+        self.current_ind = 0
+
+        def _wrapper():
+            while self.current_ind < self.num_batch:
+                start_ind = self.batch_size * self.current_ind
+                end_ind = min(self.size, self.batch_size * (self.current_ind + 1))
+                x_i = self.xs[start_ind: end_ind, ...]
+                y_i = self.ys[start_ind: end_ind, ...]
+                weather_i = self.weather[start_ind: end_ind, ...]
+                weather_i = weather_i.astype(np.float)
+                yield (x_i, y_i, weather_i)
+                self.current_ind += 1
+
+        return _wrapper()
+
+class Incident_Weather_DataLoader(object):
+    def __init__(self, xs, ys, incident, weather, batch_size, pad_with_last_sample=True):
+        """
+        :param xs:
+        :param ys:
+        :param incident:
+        :param batch_size:
+        :param pad_with_last_sample: pad with the last sample to make number of samples divisible to batch_size.
+        """
+        self.batch_size = batch_size
+        self.current_ind = 0
+        if pad_with_last_sample:
+            num_padding = (batch_size - (len(xs) % batch_size)) % batch_size
+            x_padding = np.repeat(xs[-1:], num_padding, axis=0)
+            y_padding = np.repeat(ys[-1:], num_padding, axis=0)
+            weather_padding = np.repeat(weather[-1:], num_padding, axis=0)
+            incident_padding = np.repeat(incident[-1:], num_padding, axis=0)
+
+            xs = np.concatenate([xs, x_padding], axis=0)
+            ys = np.concatenate([ys, y_padding], axis=0)
+            weather = np.concatenate([weather, weather_padding], axis=0)
+            incident = np.concatenate([incident, incident_padding], axis=0)
+
+        self.size = len(xs)
+        self.num_batch = int(self.size // self.batch_size)
+        self.xs = xs
+        self.ys = ys
+        self.weather = weather
+        self.incident = incident
+
+    def shuffle(self):
+        permutation = np.random.permutation(self.size)
+        xs, ys, weather, incident= self.xs[permutation], self.ys[permutation], self.weather[permutation], self.incident[permutation]
+        self.xs = xs
+        self.ys = ys
+        self.weather = weather
+        self.incident = incident
+
+    def get_iterator(self):
+        self.current_ind = 0
+
+        def _wrapper():
+            while self.current_ind < self.num_batch:
+                start_ind = self.batch_size * self.current_ind
+                end_ind = min(self.size, self.batch_size * (self.current_ind + 1))
+                x_i = self.xs[start_ind: end_ind, ...]
+                y_i = self.ys[start_ind: end_ind, ...]
+                weather_i = self.weather[start_ind: end_ind, ...]
+                weather_i = weather_i.astype(np.float)
+                incident_i = self.incident[start_ind: end_ind, ...]
+                incident_i = incident_i.astype(np.float)
+                yield (x_i, y_i, incident_i, weather_i)
+                self.current_ind += 1
+
+        return _wrapper()
+
 class StandardScaler():
     """
     Standard the input
@@ -192,25 +298,53 @@ def load_adj(pkl_filename, adjtype):
     return sensor_ids, sensor_id_to_ind, adj
 
 
-def load_dataset(dataset_dir, batch_size, valid_batch_size= None, test_batch_size=None, graph_embedding=None, incident=None):
+def load_dataset(dataset_dir, batch_size, valid_batch_size= None, test_batch_size=None, graph_embedding=None, incident=None, weather=None):
     data = {}
     for category in ['train', 'val', 'test']:
-        cat_data = np.load(os.path.join(dataset_dir, category + '.npz'))
+        cat_data = np.load(os.path.join(dataset_dir+'/velocity', category + '.npz'))
         data['x_' + category] = cat_data['x']
         data['y_' + category] = cat_data['y']
+
     scaler = StandardScaler(mean=data['x_train'][..., 0].mean(), std=data['x_train'][..., 0].std())
     # Data format
     for category in ['train', 'val', 'test']:
         data['x_' + category][..., 0] = scaler.transform(data['x_' + category][..., 0])
         data['y_' + category][..., 0] = scaler.transform(data['y_' + category][..., 0])
 
-    if incident == True:
+    if incident == True and weather == True:
         for category in ['train', 'val', 'test']:
-            incident_data = np.load(os.path.join(dataset_dir, "incident_"+category + '.npz'))
+            incident_data = np.load(os.path.join(dataset_dir+'/incident', "incident_"+category + '.npz'))
+            data['incident_' + category] = incident_data['x']
+            weather_data = np.load(os.path.join(dataset_dir+'/weather', "weather_"+category + '.npz'))
+            data['weather_' + category] = weather_data['x']
+        weather_scaler = StandardScaler(mean=data['weather_train'][..., 0].mean(), std=data['weather_train'][..., 0].std())
+        # Data format
+        for category in ['train', 'val', 'test']:
+            data['weather_' + category][..., 0] = weather_scaler.transform(data['weather_' + category][..., 0])
+        data['train_loader'] = Incident_Weather_DataLoader(data['x_train'], data['y_train'], data['incident_train'], data['weather_train'], batch_size)
+        data['val_loader'] = Incident_Weather_DataLoader(data['x_val'], data['y_val'], data['incident_val'], data['weather_val'], valid_batch_size)
+        data['test_loader'] = Incident_Weather_DataLoader(data['x_test'], data['y_test'], data['incident_test'], data['weather_test'], test_batch_size)
+
+    elif weather == True:
+        for category in ['train', 'val', 'test']:
+            weather_data = np.load(os.path.join(dataset_dir+'/weather', "weather_"+category + '.npz'))
+            data['weather_' + category] = weather_data['x']
+        weather_scaler = StandardScaler(mean=data['weather_train'][..., 0].mean(), std=data['weather_train'][..., 0].std())
+        # Data format
+        for category in ['train', 'val', 'test']:
+            data['weather_' + category][..., 0] = weather_scaler.transform(data['weather_' + category][..., 0])
+        data['train_loader'] = Weather_DataLoader(data['x_train'], data['y_train'], data['weather_train'], batch_size)
+        data['val_loader'] = Weather_DataLoader(data['x_val'], data['y_val'], data['weather_val'], valid_batch_size)
+        data['test_loader'] = Weather_DataLoader(data['x_test'], data['y_test'], data['weather_test'], test_batch_size)
+
+    elif incident == True:
+        for category in ['train', 'val', 'test']:
+            incident_data = np.load(os.path.join(dataset_dir+'/incident', "incident_"+category + '.npz'))
             data['incident_' + category] = incident_data['x']
         data['train_loader'] = Incident_DataLoader(data['x_train'], data['y_train'], data['incident_train'], batch_size)
         data['val_loader'] = Incident_DataLoader(data['x_val'], data['y_val'], data['incident_val'], valid_batch_size)
         data['test_loader'] = Incident_DataLoader(data['x_test'], data['y_test'], data['incident_test'], test_batch_size)
+
     else:
         data['train_loader'] = DataLoader(data['x_train'], data['y_train'], batch_size)
         data['val_loader'] = DataLoader(data['x_val'], data['y_val'], valid_batch_size)
